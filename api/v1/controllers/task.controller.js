4d8e06b5f70f403d3268e57ae4187dfb08d2243c
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-05 13:35:51 +0700

    Bộ lọc theo trạng thái

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -2,25 +2,32 @@ const Task = require("../../../models/task.model");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
-    const tasks = await Task.find({
-      deleted: false,
-    });
-  
-    res.json(tasks);
+  let find = {
+    deleted: false,
+  };
+
+  console.log(req.query);
+  if (req.query.status) {
+    find.status = req.query.status;
   }
+
+  const tasks = await Task.find(find);
+
+  res.json(tasks);
+};
 
 // [GET] /api/v1/detail/:id
 module.exports.detail = async (req, res) => {
-    try {
-      const id = req.params.id;
-  
-      const task = await Task.findOne({
-        _id: id,
-        deleted: false,
-      });
-  
-      res.json(task);
-    } catch (error) {
-      res.json("Không tìm thấy !");
-    }
+  try {
+    const id = req.params.id;
+
+    const task = await Task.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    res.json(task);
+  } catch (error) {
+    res.json("Không tìm thấy !");
   }
+};
