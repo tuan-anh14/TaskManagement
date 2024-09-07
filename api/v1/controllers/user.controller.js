4e81f56ca0968462640021b09c18a35d9a15dda2
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-07 22:18:01 +0700

    Màn hình nhập mã OTP xác thực

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -127,6 +127,41 @@ module.exports.forgotPassword = async (req, res) => {
 
   res.json({
     code: 200,
-    message: "Đã gửi mã OTP qua email !"
+    message: "Đã gửi mã OTP qua email !",
+  });
+};
+
+// [POST] /api/v1/users/password/forgot
+module.exports.otpPassword = async (req, res) => {
+  const email = req.body.email;
+  const otp = req.body.otp;
+
+  console.log(email);
+  console.log(otp);
+
+  const result = await ForgotPassword.findOne({
+    email: email,
+    otp: otp,
+  });
+
+  if (!result) {
+    res.json({
+      code: 400,
+      message: "Mã OTP không hợp lệ !",
+    });
+    return;
+  }
+
+  const user = await User.findOne({
+    email: email,
+  });
+
+  const token = user.token;
+  res.cookie("token", token);
+
+  res.json({
+    code: 200,
+    message: "Xác thực thành công !",
+    token: token,
   });
 };
