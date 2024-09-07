1d0c7b8b6ff91370d1328cbc8da114f103208eb1
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-07 11:08:45 +0700

    Tính năng đăng nhập

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const md5 = require("md5");
 const User = require("../models/user.model");
 
-// [GET] /api/v1/users/register
+// [POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
   req.body.password = md5(req.body.password);
 
@@ -35,4 +35,47 @@ module.exports.register = async (req, res) => {
       token: token,
     });
   }
+};
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(email);
+  console.log(password);
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+
+  console.log(user);
+
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại !",
+    });
+
+    return;
+  }
+
+  if (md5(password) !== user.password) {
+    res.json({
+      code: 400,
+      message: "Sai mật khẩu !",
+    });
+
+    return;
+  }
+
+  const token = user.token;
+  res.cookie("token", token);
+
+  res.json({
+    code: 200,
+    message: "Đăng nhập thành công!",
+    token: token,
+  });
 };
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../controllers/user.controller")
+const controller = require("../controllers/user.controller");
+
+// Cần thêm phần validate
 
 router.post("/register", controller.register);
+
+router.post("/login", controller.login);
 
 module.exports = router;
