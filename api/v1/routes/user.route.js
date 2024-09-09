6ec2e217a6c2bdeeca906fec2d24e7ac25f6dec4
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-09 22:44:49 +0700

    Chuyển các route thành private

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const controller = require("../controllers/user.controller");
 
+const authMiddleware = require("../middlewares/auth.middleware");
+
 // Cần thêm phần validate
 
 router.post("/register", controller.register);
@@ -15,6 +17,6 @@ router.post("/password/otp", controller.otpPassword);
 
 router.post("/password/reset", controller.resetPassword);
 
-router.get("/detail", controller.detail);
+router.get("/detail", authMiddleware.requireAuth, controller.detail);
 
 module.exports = router;
