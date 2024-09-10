662606974fa2da2f69a7b3acf0510a5651402d55
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-10 14:13:47 +0700

    Lưu id của user đã tạo task

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -155,6 +155,7 @@ module.exports.changeMulti = async (req, res) => {
 // [POST] /api/v1/create
 module.exports.create = async (req, res) => {
   try {
+    req.body.createdBy = req.user.id;
     const task = new Task(req.body);
     const data = await task.save();
 
